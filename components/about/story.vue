b960ecb51ca1537b4725f9af2d4b724fe4312345
lopze94 <lopze.94@gmail.com>
2018-08-09 19:51:41 -0600

    Edited nav on Story Section and Changed Expand logic on mission

--- components/about/story.vue
+++ components/about/story.vue
@@ -5,49 +5,60 @@
         <karl-maeser v-if="karl"></karl-maeser>
         <robert-thomas v-if="robert"></robert-thomas>
     
-        <a href="#story"><div class="card-deck" v-if="!(history||karl||robert)">
-            <div class="card border-0" v-on:click="selectCard(0)">
-                <img class="card-img-top" src="/img/about/honors_history.jpg" alt="Honors History">
-                <div class="card-img-overlay text-white">
-                    <table style="height: 100%;width: 100%;" v-if="!show2">
-                        <tbody>
-                            <tr>
-                                <td class="align-middle h5 text-center text-uppercase">Honors History</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <a href="#story">
+            <div class="card-deck" v-if="!(history||karl||robert)">
+                <div class="card border-0" v-on:click="selectCard(0)">
+                    <img class="card-img-top" src="/img/about/honors_history.jpg" alt="Honors History">
+                    <div class="card-img-overlay text-white">
+                        <table style="height: 100%;width: 100%;" v-if="!show2">
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle h5 text-center text-uppercase">Honors History</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card border-0" v-on:click="selectCard(1)">
+                    <img class="card-img-top" src="/img/about/karl_maeser.jpg" alt="Karl G. Maeser">
+                    <div class="card-img-overlay text-white">
+                        <table style="height: 100%;width: 100%;" v-if="!show2">
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle h5 text-center text-uppercase">Karl G. Maeser</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card border-0" v-on:click="selectCard(2)">
+                    <img class="card-img-top" src="/img/about/robert_thomas.jpg" alt="Robert K. Thomas">
+                    <div class="card-img-overlay text-white">
+                        <table style="height: 100%;width: 100%;" v-if="!show2">
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle h5 text-center text-uppercase">Robert K. Thomas</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="card border-0" v-on:click="selectCard(1)">
-                <img class="card-img-top" src="/img/about/karl_maeser.jpg" alt="Karl G. Maeser">
-                <div class="card-img-overlay text-white">
-                    <table style="height: 100%;width: 100%;" v-if="!show2">
-                        <tbody>
-                            <tr>
-                                <td class="align-middle h5 text-center text-uppercase">Karl G. Maeser</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        </a>
+        <nav class="nav nav-pills nav-justified" v-if="history||karl||robert">
+                <div class="col-sm-6 col-lg-3">
+                    <a class="nav-item nav-link" :class="{active: history}" href="#story" v-on:click="selectCard(0)">Honors History</a>
                 </div>
-            </div>
-            <div class="card border-0" v-on:click="selectCard(2)">
-                <img class="card-img-top" src="/img/about/robert_thomas.jpg" alt="Robert K. Thomas">
-                <div class="card-img-overlay text-white">
-                    <table style="height: 100%;width: 100%;" v-if="!show2">
-                        <tbody>
-                            <tr>
-                                <td class="align-middle h5 text-center text-uppercase">Robert K. Thomas</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="col-sm-6 col-lg-3">
+                    <a class="nav-item nav-link" :class="{active: karl}" href="#story" v-on:click="selectCard(1)">Karl G. Maeser</a>
                 </div>
-            </div>
-        </div></a>
-        <a href="#story"><nav class="nav nav-pills nav-justified pb-2" v-if="history||karl||robert">
-            <button class="nav-item nav-link btn btn-outline-primary m-1" v-on:click="selectCard(left.select)">{{left.text}}</button>
-            <button class="nav-item nav-link btn btn-outline-primary m-1" v-on:click="selectCard(right.select)">{{right.text}}</button>
-            <button class="nav-item nav-link btn btn-secondary m-1" v-on:click="reset">Return</button>
-        </nav></a>
+                <div class="col-sm-6 col-lg-3">
+                    <a class="nav-item nav-link" :class="{active: robert}" href="#story" v-on:click="selectCard(2)">Robert K. Thomas</a>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <a class="nav-item nav-link disabled" href="#story" v-on:click="reset">Return</a>
+                </div>
+        </nav>
     </div>
 </template>
 
@@ -86,26 +97,14 @@
                     case 0:
                         this.selected = "Honors History";
                         this.history = true;
-                        this.left.text = "Karl G. Maeser";
-                        this.left.select = 1;
-                        this.right.text = "Robert K. Thomas";
-                        this.right.select = 2;
                         break;
                     case 1:
                         this.selected = "Karl G. Maeser";
                         this.karl = true;
-                        this.left.text = "Honors History";
-                        this.left.select = 0;
-                        this.right.text = "Robert K. Thomas";
-                        this.right.select = 2;
                         break;
                     case 2:
                         this.selected = "Robert K. Thomas";
                         this.robert = true;
-                        this.left.text = "Honors History";
-                        this.left.select = 0;
-                        this.right.text = "Karl G. Maeser";
-                        this.right.select = 1;
                         break;
                 }
             },
@@ -120,9 +119,6 @@
 </script>
 
 <style scoped>
-    a{
-        text-decoration: none;
-    }
     .card {
         box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
     }
