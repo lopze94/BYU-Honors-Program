63eaf6415178028d5f239cd87eb82103e361986b
lopze94 <lopze.94@gmail.com>
2018-08-15 16:43:42 -0600

    Update server update script

--- push.sh
+++ push.sh
@@ -1,11 +1,6 @@
 echo What is your commit message?
-
 read commitmessage
-
 git add -A
-echo $commitmessage
 git commit -m "$commitmessage"
-
 git push
-
 echo Your changes have been pushed to the repository with the message \"$commitmessage\"
--- updatekapok.sh
+++ updatekapok.sh
@@ -1,4 +1,12 @@
-echo Updating Kapok... What is your merge message?
-read mergemessage
+echo Opening nuxt_honors folder
+cd /var/www/kapok.ventures/html/nuxt_honors
+echo Updating Kapok... Pulling new changes from repositories
 git pull
-
+echo Stoping all services
+forever stopall
+echo Building...
+npm run build
+echo restarting services
+source .env
+forever start server.js
+forever start server/index.js
