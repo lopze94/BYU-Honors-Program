9b9a14e2aec8ee8a5a385eb0c64ef646d6227816
lopze94 <lopze.94@gmail.com>
2018-08-15 15:31:22 -0600

    Edited deadlines

--- components/currentstudents/Deadlines.vue
+++ components/currentstudents/Deadlines.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="container jumptarget pb-4" id="deadlines">
     <h2 class="py-3">Deadlines</h2>
-    <p>Please, select your graduation date to view the deadlines.</p>
+    <p></p>
     <div class="row">
-      <div class="col-sm-12 col-md-4 pb-3">
+      <div class="col-sm-12 col-md-4 col-lg-3 pb-3">
         <div class="list-group" id="list-tab" role="tablist">
-          <a class="list-group-item list-group-item-action active" id="list-april-list" data-toggle="list" href="#list-april" role="tab" aria-controls="april">April 2019</a>
+          <a class="list-group-item list-group-item-action active" id="list-select-list" data-toggle="list" href="#list-select" role="tab" aria-controls="select">Select Graduation</a>
+          <a class="list-group-item list-group-item-action" id="list-april-list" data-toggle="list" href="#list-april" role="tab" aria-controls="april">April 2019</a>
           <a class="list-group-item list-group-item-action" id="list-june-list" data-toggle="list" href="#list-june" role="tab" aria-controls="june">June 2019</a>
           <a class="list-group-item list-group-item-action" id="list-august-list" data-toggle="list" href="#list-august" role="tab" aria-controls="august">August 2019</a>
           <a class="list-group-item list-group-item-action" id="list-december-list" data-toggle="list" href="#list-december" role="tab" aria-controls="december">December 2018</a>
         </div>
       </div>
-      <div class="col-sm-12 col-md-8">
+      <div class="col-sm-12 col-md-8 col-lg-9">
         <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="list-select" role="tabpanel" aria-labelledby="list-select-list">
+            <div class="jumbotron">
+              <h1 class="display-4">Don't stress!</h1>
+              <p class="lead">Select your graduation date to view the deadlines taylored to your timeline. Click a deadline to read more about it.</p>
+            </div>
+          </div>
           <div v-for="(list, i) in deadlines" :key="i" class="tab-pane fade show" :id="'list-'+list.graduation" role="tabpanel" :aria-labelledby="'list-'+list.graduation+'-list'">
             <table class="table table-hover table-borderles rounded table-sm">
               <caption>Graduating in {{list.graduation | capitalize}} 2019</caption>
@@ -42,8 +49,8 @@
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalCenterTitle">{{event.title}}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+                  <span aria-hidden="true">&times;</span>
+                </button>
           </div>
           <div class="modal-body">
             {{event.info}}
@@ -127,3 +134,9 @@
     }
   }
 </script>
+
+<style scoped>
+  .table-hover {
+    cursor: pointer;
+  }
+</style>
