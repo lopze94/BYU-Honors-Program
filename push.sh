14222cda7466ad94a109d4a862313a82ec533ff5
lopze94 <lopze.94@gmail.com>
2018-08-15 17:19:06 -0600

    Fixed untracked files

--- push.sh
+++
@@ -1,6 +0,0 @@
-echo What is your commit message?
-read commitmessage
-git add -A
-git commit -m "$commitmessage"
-git push
-echo Your changes have been pushed to the repository with the message \"$commitmessage\"
