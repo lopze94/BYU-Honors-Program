f3ce0e0ccf8ed69d12b60faff02241e31e69244c
Josué López <lopze94@BYUHonosProgram.app.byu.edu>
2018-08-17 12:22:52 -0600

    Edited graph to show values

--- components/currentstudents/StudentsByCollege.vue
+++ components/currentstudents/StudentsByCollege.vue
@@ -1,12 +1,17 @@
 <template>
+<div>
   <div class="container">
       <h3>Students by College</h3>
+  </div>
+  <div class="container pl-sm-0 pl-md-3">
     <bar-chart :chart-data="datacollection"
     :options="options"
     :width="500"
-  :height="200">
-  </bar-chart>
+    :height="200"
+    >
+    </bar-chart>
   </div>
+</div>
 </template>
 
 <script>
@@ -45,6 +50,36 @@ export default {
               }
             }
           ]
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function() {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+
+            ctx.font = Chart.helpers.fontString(
+              Chart.defaults.global.defaultFontSize,
+              Chart.defaults.global.defaultFontStyle,
+              Chart.defaults.global.defaultFontFamily
+            );
+            ctx.textAlign = "left";
+            ctx.textBaseline = "middle";
+
+            this.data.datasets.forEach(function(dataset, i) {
+              ctx.fillStyle = "#292929";
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                ctx.fillText(data, bar._model.x + 5, bar._model.y);
+              });
+            });
+          }
+        },
+        tooltips: {
+          enabled: false
         }
       }
     };
