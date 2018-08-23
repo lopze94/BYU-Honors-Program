47d07a5f2d486f8c2002f6d8ab8452975269e922
Josue Lopez <lopze.94@gmail.com>
2018-08-23 19:32:13 +0000

    changes from phone

--- components/global/events.vue
+++ components/global/events.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container jumptarget" id="events" ssr-only>
         <h2 class="py-3">Upcoming Events</h2>
-        <byu-calendar categories="10" days="90" display="4" limit="3">
+        <byu-calendar categories="1100" days="90" display="4" limit="3">
         </byu-calendar>
     </div>
 </template>
